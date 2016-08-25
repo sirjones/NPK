41e588b800be5e7642e4c49ab91c024a63a95520
Sofia Chardin <sofiachardin@gmail.com>
2016-08-25 11:18:14 -0300

    img, js, etc

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 var tamañoGrande;
-window.addEventListener("orientationchange", function() {
-	// Announce the new orientation number
-	alert(screen.orientation);
-}, false);
-  // Start of StatCounter Code for Default Guide
-  var sc_project=11049991; 
-  var sc_invisible=1; 
-  var sc_security="10f8c238"; 
-  var scJsHost = (("https:" == document.location.protocol) ?
-  "https://secure." : "http://www.");
-  document.write("<sc"+"ript type='text/javascript' src='" +
-  scJsHost+
-  "statcounter.com/counter/counter.js'></"+"script>");
-    // End of StatCounter Code for Default Guide 
+/*window.addEventListener("orientationchange", function() {	alert(screen.orientation); }, false);*/
+// Start of StatCounter Code for Default Guide 
+var sc_project=11049991; 
+var sc_invisible=1; 
+var sc_security="10f8c238"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+// End of StatCounter Code for Default Guide
+
 $(document).ready(function() {
   $('#intro').addClass('hi');
  	$('#equipo, #titulovideo, #wrapperWrapper, #titulovideo, .imgSobrenpk, .text1, .textPU, #contactos, #contact').css('opacity', 0);
@@ -134,5 +132,3 @@ $('.fancybox').fancybox({
 			  	},
 		 },
 	 });
-
-
